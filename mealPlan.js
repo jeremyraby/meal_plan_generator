244771383308736bcfd12ecdd8a3c1069a83bea7
jeremyraby <jeremy.raby10@gmail.com>
2024-10-10 18:26:21 -0300

    Add recipes from `links.md`

--- mealPlan.js
+++ mealPlan.js
@@ -2,7 +2,11 @@
 const breakfasts = [
   { name: "one pan huevos rancheros", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/huevosRancheros.pdf" },
   { name: "crockpot sausage & cheese casserole", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/sausageCheeseBreakfastCasserole.pdf" },
-  //{ name: "egg muffin", link: "recipes/egg_muffin.html" },
+  { name: "egg muffin", link: "recipes/egg_muffin.html" },
+  { name: "Cottage Cheese Breakfast Bowls 6 Ways", link: "https://web.archive.org/web/20201030003056/https://www.budgetbytes.com/cottage-cheese-breakfast-bowls-6-ways/" },
+  { name: "Country Breakfast Bowls (Freezable)", link: "https://web.archive.org/web/20201030003225/https://www.budgetbytes.com/country-breakfast-bowls-freezable/" },
+  { name: "Make-Ahead Microwave Breakfast Scrambles", link: "https://web.archive.org/web/20201030003430/https://www.budgetbytes.com/make-ahead-microwave-breakfast-scrambles/" },
+  { name: "Smoothie Packs: My Favorite Smoothie", link: "https://web.archive.org/web/20201030003612/https://www.budgetbytes.com/smoothie-packs-my-favorite-smoothie/" },
   // Add more breakfasts with corresponding links
 ];
 
@@ -16,7 +20,14 @@ const lunches = [
 const dinners = [
   { name: "beef broccoli ramen stir-fry", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/beefBroccoliRamenStirFry.pdf" },
   { name: "chicken with creamy cilantro lime sauce", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/chickenCreamyCilantroLimeSauce.pdf" },
-  //{ name: "chicken parmesan pasta bake", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/chickenParmesanPastaBake.pdf" },
+  { name: "chicken parmesan pasta bake", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/chickenParmesanPastaBake.pdf" },
+  { name: "Skillet Meatballs and Marinara (Sunday Meal Prep)", link: "https://web.archive.org/web/20201030002827/https://www.budgetbytes.com/sunday-meal-prep-skillet-meatballs-and-marinara/" },
+  { name: "Jerk Chicken with Pineapple Black Bean Salsa", link: "https://web.archive.org/web/20201030003729/https://www.budgetbytes.com/jerk-chicken-with-pineapple-black-bean-salsa/" },
+  { name: "Cilantro Lime Chicken Meal (Sunday Meal Prep)", link: "https://web.archive.org/web/20201030003910/https://www.budgetbytes.com/sunday-meal-prep-cilantro-lime-chicken-meal/" },
+  { name: "The Pizza Roll Up Lunch Box", link: "https://web.archive.org/web/20201030005240/https://www.budgetbytes.com/the-pizza-roll-up-lunch-box/" },
+  { name: "Loaded Sweet Potatoes with Chipotle Lime Crema", link: "https://web.archive.org/web/20201030011045/https://www.budgetbytes.com/loaded-sweet-potatoes-with-chipotle-lime-crema/" },
+  { name: "Sweet Potato Taco Meal Prep", link: "https://web.archive.org/web/20201030011211/https://www.budgetbytes.com/sweet-potato-taco-meal-prep/" },
+  { name: "Pesto Stuffed Shells Meal Prep", link: "https://web.archive.org/web/20201030011326/https://www.budgetbytes.com/pesto-stuffed-shells-meal-prep/" },
   // Add more dinners with corresponding links
 ];
 
