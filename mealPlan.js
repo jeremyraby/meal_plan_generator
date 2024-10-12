1cdd4abe82f91c4111172541d43d1084839e27bf
jeremyraby <jeremy.raby10@gmail.com>
2024-10-11 21:19:17 -0500

    Add links to recipes

--- mealPlan.js
+++ mealPlan.js
@@ -6,6 +6,8 @@ const breakfasts = [
   { name: "PB & J Cottage Cheese Breakfast Bowl", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Peanut%20Butter%20and%20Jelly%20Cottage%20Cheese%20Breakfast%20Bowl%20-%20Budget%20Bytes.pdf" },
   { name: "Microwave Breakfast Scrambles", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Make%20Ahead%20Microwave%20Breakfast%20Scrambles%20-%20Budget%20Bytes.pdf" },
   { name: "Country Breakfast Bowls", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Country%20Breakfast%20Bowls%20-%20Budget%20Bytes.pdf" },
+  { name: "Butternut Squash & Brussels Sprout Breakfast Quesadillas", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Butternut%20and%20Brussels%20Sprout%20Breakfast%20Quesadillas%20-%20Domesticate%20ME.pdf" },
+  { name: "Shakshuka", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Shakshuka%20-%20Dessert%20for%20Two.pdf" },
   // Add more breakfasts with corresponding links
 ];
 
@@ -27,7 +29,17 @@ const dinners = [
   { name: "Sweet Potato Tacos", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Sweet%20Potato%20Taco%20Meal%20Prep%20-%20Budget%20B.md" },
   { name: "Pizza Roll Ups", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/The%20Pizza%20Roll%20Up%20Lunch%20Box%20-%20Budget%20Bytes.pdf" },
   { name: "Sweet Potatoes with Lime Crema", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Loaded%20Sweet%20Potatoes%20with%20Lime%20Crema%20-%20Budget%20Bytes.pdf" },
-  { name: "Jerk Chicken with Pineapple Black Bean Salsa", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Jerk%20Chicken%20with%20Pineapple%20Black%20Bean%20Salsa%20-%20Budget%20Bytes.pdf" },
+  { name: "Ground Beef Tacos", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Ground%20Beef%20for%20Tacos%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Graham Cracker Fried Chicken", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Graham%20Cracker%20Fried%20Chicken%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Skillet Chicken Pot Pie", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Chicken%20Pot%20Pie%20Skillet%20Recipe%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Broccoli Rice Casserole with Chicken", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Broccoli%20Rice%20Casserole%20with%20Chicken%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Baked Breaded Chicken Strips (Freezer)", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Baked%20Breaded%20Chicken%20Strips%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Bacon Wrapped Aspargus", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Bacon%20Wrapped%20Asparagus%20-%20Dessert%20for%20Two.pdf" },
+  { name: "PB Steak Tacos", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Peanut%20Butter%20Steak%20Tacos%20(with%20sriracha!)%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Pork Meatballs", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Pork%20Meatballs%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Skillet Lasagna", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Skillet%20Lasagna%20-%20Dessert%20for%20Two.pdf" },
+  { name: "Stuffed Bell Peppers", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/Stuffed%20Peppers%20for%20Two%20-%20Dessert%20for%20Two.pdf" },
+  { name: "White Chicken & Green Chile Enchiladas", link: "https://github.com/jeremyraby/meal_plan_generator/blob/main/White%20Chicken%20Enchilada%20Recipe%20-%20Dessert%20for%20Two.pdf" },
   // Add more dinners with corresponding links
 ];
 
